b017469cf3f903eb197403869b2458b1cba281ac
PashanIrani <pashanirani@gmail.com>
2018-10-17 21:22:12 -0700

    progress has been mad, yet again most time was spent understanding stuff, rather than implementation

--- CNN.py
+++ CNN.py
@@ -1,15 +1,23 @@
 import tensorflow as tf
+import numpy as np
 import os
 import pickle
 
 IMAGE_SIZE = 32
-
-# Global constants describing the CIFAR-10 data set.
+IMAGE_SIZE_CROPPED = 24
+NUM_CHANNELS = 3
 NUM_CLASSES = 10
-NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
-NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
-
-DATA = [] # holds images
+CLASSES = ['airplane',
+ 'automobile',
+ 'bird',
+ 'cat',
+ 'deer',
+ 'dog',
+ 'frog',
+ 'horse',
+ 'ship',
+ 'truck']
+DATA = []  # holds images
 
 def unpickle(file):
     with open(file, 'rb') as fo:
@@ -17,38 +25,70 @@ def unpickle(file):
     return dict
 
 def setup():
-
-    filenames = [os.path.join('data/cifar-10-batches-bin', 'data_batch_%d' % i)
+    filenames = [os.path.join('data', 'data_batch_%d' % i)
                  for i in range(1, 6)]
 
     # takes each batch file and adds it to array
     for dir in filenames:
         DATA.append(unpickle(dir))
 
+    print("completed setup")
+
 
 def train():
-    # todod
-    print('train')
-    print(DATA[0][b'data'][0])
-    image = tf.convert_to_tensor(DATA[0][b'data'][0], dtype=tf.float64)
-    input = [image, IMAGE_SIZE, IMAGE_SIZE, 3]
-    tf.nn.conv2d(input, [IMAGE_SIZE, IMAGE_SIZE, 3, 1], strides=[1, 1, 1, 1],
-                     padding='SAME')
-    print('lol')
+    tf.reset_default_graph()
+    data = DATA
+    x = DATA[0][b'data'].astype(np.float32)
+    l = DATA[0][b'labels']
+    imageNum = 566
+
+    p = network(x, l, training=True)
+    print(p)
+
+    sess = tf.Session()
+
+    sess.run()
+    # print(loss)
+
+
+def network(images, labels, training=False):
+
+    input_layer = tf.reshape(images, [-1, IMAGE_SIZE, IMAGE_SIZE, 3])
+
+    conv1 = tf.layers.conv2d(
+        inputs=input_layer,
+        filters=32,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
+
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=64,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
+
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+
+    pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+
+    dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+
+    dropout = tf.layers.dropout(
+        inputs=dense, rate=0.4, training=training == tf.estimator.ModeKeys.TRAIN)
+
+    # Logits Layer
+    logits = tf.layers.dense(inputs=dropout, units=10)
+
+    print(logits)
+
+    # return y_pred, loss
 
 
 
 setup()
 train()
 
-
-
-
-
-
-
-
-#filename_queue = tf.train.string_input_producer(filenames)
-
-# record bytes are the size of the image,  32 for height and weight, 3 for number of channels (rgb), and 1 for label
-#key, value = tf.FixedLengthRecordReader(record_bytes=(32 * 32 * 3) + 1).read(filename_queue)
